e6106fd2874de5f82d047eb0fe0b711500434469
Robert Fletcher <lobatifricha@gmail.com>
2011-06-07 20:44:32 -0700

    first commit, weve got user login

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Website::Application.config.secret_token = '32212439d4a813a47178985b607ca59fcb2f5f9050155ff8d63c64984e616f9575107647dbc762958495d9fdb58720003f5b8e3f3fb4710fd638fc0659725988'
