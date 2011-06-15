194a6b651f3359d27ad7dadafd43d8203cbb4244
phil303 <philaquilina@gmail.com>
2011-06-14 20:18:03 -0700

    Added email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,13 @@ class User < ActiveRecord::Base
   has_many :comments, :as => :author
 
   attr_accessible :username, :email, :password, :password_confirmation
+  
   attr_accessor :password
 
   before_save :encrypt_password
 
   validates_presence_of :username, :email
+  validates :email, :format => { :with => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates_uniqueness_of :username, :email
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
