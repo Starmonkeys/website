2111923cda7978105dee036c8cba855123d89a07
layla <laylamandella@gmail.com>
2011-06-14 21:48:49 -0700

    task edit page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -26,6 +26,7 @@ class TasksController < ApplicationController
 
   def edit
     @task = Task.find(params[:id])
+    @project = Project.find(params[:project_id])
     redirect_to tasks_path unless @task.author == current_user
   end
 
